2800be3cca8cd9707202913a3943f5b5d9399866
AnnaSunny2011 <81269789+AnnaSunny2011@users.noreply.github.com>
2021-03-23 19:44:10 -0400

    Add files via upload

---
+++ anya.js
@@ -0,0 +1,22 @@
+alert("Hey! I am Anya!");
+alert("And in this webpage, you will get to know me, Anya!");
+alert("I want to meet you too a little,so you will meet me, Anya and I will meet you!");
+var name = prompt("Whats your name?");
+var age = prompt ("Whats your age?");
+var share = prompt("Now, you, " + name + "... that is " + age + "years old, will tell me about you! Isn't it cool?");
+
+if ( age == 9 ) {
+	alert("Wow...		we both are 9 years old!");
+
+}
+ else {
+	alert("Good! I'm 9 years old");
+	
+ }
+ 
+	if (age == 10 ) {
+		
+		alert ("You are such an amazing " + age + "year old 'kid'!"); 
+}
+	
+alert("Thanks for your share! Is it " + share + "?");
